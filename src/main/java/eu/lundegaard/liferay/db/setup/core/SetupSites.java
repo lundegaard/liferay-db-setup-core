8ce189611dfd1d8325e7e878d60fd696fe9d1159
Roman Srom (Portal specialist) <raptor.army@seznam.cz>
2020-04-28 11:28:18 +0200

    fix: log level changed to info, because it is not error message

--- src/main/java/eu/lundegaard/liferay/db/setup/core/SetupSites.java
+++ src/main/java/eu/lundegaard/liferay/db/setup/core/SetupSites.java
@@ -12,10 +12,10 @@ package eu.lundegaard.liferay.db.setup.core;
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -319,7 +319,7 @@ public class SetupSites {
     static void setCustomFields(final long runAsUserId, final long groupId,
                                 final long company, final Site site) {
         if (site.getCustomFieldSetting() == null || site.getCustomFieldSetting().isEmpty()) {
-            LOG.error("Site does has no Expando field settings.");
+            LOG.info("Site does has no Expando field settings.");
         } else {
             Class clazz = com.liferay.portal.kernel.model.Group.class;
             String resolverHint = "Resolving customized value for page " + site.getName() + " "
